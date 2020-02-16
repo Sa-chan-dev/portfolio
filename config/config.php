39407cc6767a98b500259837404b5b9609c114c8
Delaunay <delaunay.s50@gmail.com>
2020-02-16 14:15:32 +0100

    modif config

--- config/config.php
+++ config/config.php
@@ -2,19 +2,19 @@
 /**
  * CONFIGURATION
  */
-const DB_SGBD = 'mysql';
-const DB_SGBD_URL = 'localhost';
-const DB_DATABASE = 'mon_portefolio';
-const DB_CHARSET = 'utf8';
-const DB_USER = 'admin';
-const DB_PASSWORD = 'admin';
-
 // const DB_SGBD = 'mysql';
 // const DB_SGBD_URL = 'localhost';
 // const DB_DATABASE = 'mon_portefolio';
 // const DB_CHARSET = 'utf8';
-// const DB_USER = 'root';
-// const DB_PASSWORD = '';
+// const DB_USER = 'admin';
+// const DB_PASSWORD = 'admin';
+
+const DB_SGBD = 'mysql';
+const DB_SGBD_URL = 'localhost';
+const DB_DATABASE = 'mon_portefolio';
+const DB_CHARSET = 'utf8';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
 
 
 /** FILES */
